baec7af3305068d759d7b9f752d996236cb3dcff
Aleksi Backman <bddubbb@gmail.com>
2022-01-17 18:58:35 +0200

    server: Start waiting for connections

--- src/server/main.c
+++ src/server/main.c
@@ -4,9 +4,38 @@
 
 #include "config.h"
 
-int main(void) {
+int load_certificate_and_private_key(SSL_CTX* ctx)
+{
+	int ret = 0;
+
+	ret = SSL_CTX_use_certificate_file(ctx, CFG_CERT_NAME, SSL_FILETYPE_PEM);
+	if (ret <= 0) {
+		ERR_print_errors_fp(stderr);
+		return 1;
+	}
+
+	ret = SSL_CTX_use_PrivateKey_file(ctx, CFG_PRIVKEY_PATH, SSL_FILETYPE_PEM);
+	if (ret <= 0) {
+		ERR_print_errors_fp(stderr);
+		return 1;
+	}
+
+	ret = SSL_CTX_check_private_key(ctx);
+	if (ret <= 0) {
+		ERR_print_errors_fp(stderr);
+		return 1;
+	}
+
+	return 0;
+}
+
+int main(void)
+{
 	const SSL_METHOD* method;
+	SSL* ssl = 0;
 	SSL_CTX* ctx;
+	BIO *ssl_bio = 0;
+	BIO *accept_bio = 0;
 	int ret = 0;
 
 	method = TLS_server_method();
@@ -17,13 +46,94 @@ int main(void) {
 		return 1;
 	}
 
-	ret = SSL_CTX_use_certificate_file(ctx, CFG_CERT_NAME, SSL_FILETYPE_PEM);
+	ret = load_certificate_and_private_key(ctx);
+	if (0 != ret) {
+		return 1;
+	}
+
+	ssl = SSL_new(ctx);
+	if (0 == ssl) {
+		perror("Failed to create new SSL structure");
+		ERR_print_errors_fp(stderr);
+		return 1;
+	}
+
+	// ssl_bio = BIO_new_ssl(ctx, 0 /* set as server */);
+	ssl_bio = BIO_new(BIO_f_ssl());
+	if (0 == ssl_bio) {
+		perror("Failed to allocate new SSL BIO");
+		ERR_print_errors_fp(stderr);
+		return 1;
+	}
+
+	accept_bio = BIO_new(BIO_s_accept());
+	// accept_bio = BIO_new_accept("2424");
+	if (0 == ssl_bio) {
+		perror("Failed to allocate new accept BIO");
+		ERR_print_errors_fp(stderr);
+		return 1;
+	}
+
+	// https://linux.die.net/man/3/bio_set_close
+	ret = BIO_set_ssl(ssl_bio, ssl, BIO_NOCLOSE);
 	if (ret <= 0) {
+		perror("Failed to set SSL to SSL BIO");
+		ERR_print_errors_fp(stderr);
+		return 1;
+	}
+
+	ret = BIO_set_ssl_mode(ssl_bio, 0 /* set server mode */);
+	if (ret <= 0) {
+		perror("Failed to set SSL mode");
+		ERR_print_errors_fp(stderr);
+		return 1;
+	}
+
+	ret = BIO_set_accept_name(accept_bio, "localhost:2424");
+	if (ret <= 0) {
+		perror("Failed to set accept_name");
+		ERR_print_errors_fp(stderr);
+		return 1;
+	}
+
+	ret = BIO_set_nbio_accept(accept_bio, 0 /* set to blocking mode */);
+	if (ret <= 0) {
+		perror("Failed to set BIO to blocking");
+		ERR_print_errors_fp(stderr);
+		return 1;
+	}
+
+	ret = BIO_set_accept_bios(accept_bio, ssl_bio);
+	if (ret <= 0) {
+		perror("Failed to set SSL BIO to be duplicated"
+				" for each accepted connection");
+		ERR_print_errors_fp(stderr);
+		return 1;
+	}
+
+	ret = BIO_do_accept(accept_bio);
+	if (ret <= 0) {
+		perror("Failed to create and bind accept socket");
+		ERR_print_errors_fp(stderr);
+		return 1;
+	}
+	printf("Bound to address: %s:%s\n",
+			BIO_get_accept_name(accept_bio),
+			BIO_get_accept_port(accept_bio));
+
+	printf("Waiting for connection\n");
+	fflush(stdout);
+	ret = BIO_do_accept(accept_bio);
+	if (ret <= 0) {
+		perror("Failed to create and bind accept socket");
 		ERR_print_errors_fp(stderr);
 		return 1;
 	}
 
 	printf("Exiting\n");
+	BIO_free_all(accept_bio); // Free chain set by BIO_set_accept_bios()
+	SSL_free(ssl);
+	SSL_CTX_free(ctx);
 
 	return 0;
 }
