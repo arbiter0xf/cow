910759caebb3577a446f615710f965f556bd13c3
Aleksi Backman <bddubbb@gmail.com>
2022-01-18 11:59:36 +0200

    client: After connecting, double check cert verification

--- src/client/main.c
+++ src/client/main.c
@@ -11,6 +11,7 @@ int main(void) {
 	BIO* ssl_bio = 0;
 	SSL* ssl = 0;
 	SSL_CTX* ctx = 0;
+	X509* server_certificate = 0;
 	int ret = -1;
 
 #if OPENSSL_VERSION_NUMBER < 0x10100000L
@@ -67,9 +68,32 @@ int main(void) {
 		return 1;
 	}
 
+	server_certificate = SSL_get_peer_certificate(ssl);
+	if (0 == server_certificate) {
+		perror("Failed to get certificate from server");
+		goto fail;
+	}
+
+	ret = SSL_get_verify_result(ssl);
+	if (X509_V_OK != ret) {
+		perror("Server certificate verification failed.");
+		perror("NOTE: This is expected to be caught earlier and may ");
+		perror("indicate changed implementation or a bug.");
+		goto fail;
+	}
+
 	printf("Exiting\n");
+	BIO_ssl_shutdown(connect_bio);
 	BIO_free_all(connect_bio); // Free chain set by BIO_push()
 	SSL_CTX_free(ctx);
 
 	return 0;
+
+fail:
+	BIO_ssl_shutdown(connect_bio);
+fail_no_connection:
+	BIO_free_all(connect_bio); // Free chain set by BIO_push()
+fail_no_bio_chain:
+	SSL_CTX_free(ctx);
+	return 1;
 }
