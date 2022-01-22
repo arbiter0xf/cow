95fc6afaa8103793bc5d7aa0c3e8bc6645b56b22
Aleksi Backman <bddubbb@gmail.com>
2022-01-22 11:21:50 +0200

    Test adding maximum amount of data to item

--- test/utest_formulate_item.c
+++ test/utest_formulate_item.c
@@ -27,3 +27,20 @@ Test(formulate_item, data_is_appended_to_item)
 
 	cr_expect_str_eq(test_item.data, test_data_result);
 }
+
+Test(formulate_item, maximum_amount_of_data_is_appended_to_item)
+{
+	struct item test_item;
+	const char* test_data = "abcd1234"; // Append 8 bytes at a time
+	const int data_max = ITEM_SIZE / strlen(test_data);
+
+	item_initialize(&test_item);
+
+	for (int i = 0; i < data_max - 1; i++) {
+		item_append(&test_item, test_data);
+	}
+
+	cr_expect(8 == test_item.unused_bytes);
+	item_append(&test_item, test_data);
+	cr_expect(0 == test_item.unused_bytes);
+}
